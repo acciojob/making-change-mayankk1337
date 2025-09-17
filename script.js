b898c47e081c2a144d9e5f5b20d95c9e334329b2
mayankk1337 <ritiksingh2428@gmail.com>
2025-09-17 03:21:56 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+	
+  amount = Number(amount);
+
+  let change = { q:0, d:0, n:0, p:0 };
+
+  change.q = Math.floor(amount/25);
+  amount = amount%25;
+
+  change.d = Math.floor(amount/10);
+  amount = amount%10;
+
+  change.n = Math.floor(amount/5);
+  amount = amount%5;
+
+  change.p = amount;
+
+  return change;
+}
+
+// Using prompt input
+const c = prompt("Enter c: ");
+alert(JSON.stringify(makeChange(c)));
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
